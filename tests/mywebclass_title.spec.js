999f671fdd65a3e69cf9c1354e1f200d81d0cd04
kpw6 <kpw6@njit.edu>
2023-03-24 14:49:21 -0400

    changes and dev

--- tests/mywebclass_title.spec.js
+++ tests/mywebclass_title.spec.js
@@ -1,8 +1,8 @@
 // @ts-check
 const { test, expect } = require('@playwright/test')
 
-test('Should have MyWebClass.org title', async ({ page }) => {
+test('Should have Kevin and Lucas in title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await page.goto('http://localhost:3000')
-  await expect(page).toHaveTitle('MyWebClass.org')
+  await expect(page).toHaveTitle('Kevin and Lucas')
 })
